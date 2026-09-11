69be0ef03b8e6427b36892cc4b43494b643feb91
Yuxin.Duan <yuxin.duan.olinda@gmail.com>
2026-09-11 17:38:54 +0800

    fix for deploy

--- config/asgi.py
+++ config/asgi.py
@@ -4,19 +4,20 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import chat.routing
-
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
     'config.settings'
 )
 
-django_asgi_app = get_asgi_application()
+django_asgi_application = get_asgi_application()
+
+import chat.routing
+
 
 application = ProtocolTypeRouter(
     {
-        'http': django_asgi_app,
+        'http': django_asgi_application,
         'websocket': AuthMiddlewareStack(
             URLRouter(
                 chat.routing.websocket_urlpatterns
--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,8 +163,9 @@ STORAGES = {
     },
     'staticfiles': {
         'BACKEND': (
-            'whitenoise.storage.'
-            'CompressedManifestStaticFilesStorage'
+            'django.contrib.staticfiles.storage.StaticFilesStorage'
+            if 'test' in sys.argv
+            else 'whitenoise.storage.CompressedManifestStaticFilesStorage'
         ),
     },
 }
